ec110efd6a04ef714d1a8edf47b3b57ace1bf579
Mwangi-Derrick <mwangiderrick640@gmail.com>
2024-01-04 15:38:32 +0300

    final commit

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+  printf("Hello World!\n");
+  printf("I am learning C.\n");
+  printf("And it is awesome!\n");
+  int number = 2;
+  float decimal = 4.7;
+  char character = '3';
+  printf("%d\n", number);
+   printf("%i\n", number);
+    printf("%c\n", character);
+     printf("%f\n", decimal);
+  return 0;
+}
